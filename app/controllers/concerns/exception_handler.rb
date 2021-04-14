5be5f48a91297c3a2cb6941f23c7e688c09c1956
Francisco Paradella <franciscoleite.dev@protonmail.com>
2021-04-14 20:05:42 -0300

    Create method and more tests

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,18 @@
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({ message: e.message }, :not_found)
+    end
+  end
+
+  private
+
+  # JSON response with message; Status code 422 - unprocessable entity
+  def four_twenty_two(e)
+    json_response(object: { message: e.message }, status: :unprocessable_entity)
+  end
+end
---
+++ app/controllers/concerns/response.rb
@@ -0,0 +1,6 @@
+# Response with JSON and HTTP status code(200 default)
+module Response
+  def json_response(object:, status: :ok)
+    render json: object, status: status
+  end
+end
